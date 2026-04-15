7ee1a6930ce55986882dfd82e2ffd4e1fafdf609
Gabriel <gabrielfurtunato101@gmail.com>
2026-04-15 16:12:20 -0300

    4 Sprint do projeto

--- Projeto/database/seeders/DatabaseSeeder.php
+++ Projeto/database/seeders/DatabaseSeeder.php
@@ -5,21 +5,34 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name'     => 'Almoxarife',
+            'email'    => 'almoxarife@senai.br',
+            'password' => Hash::make('senai123'),
+            'role'     => 'almoxarife',
         ]);
+
+        User::create([
+            'name'     => 'Coordenador 1',
+            'email'    => 'coordenador1@senai.br',
+            'password' => Hash::make('senai123'),
+            'role'     => 'coordenador',
+        ]);
+
+        User::create([
+            'name'     => 'Coordenador 2',
+            'email'    => 'coordenador2@senai.br',
+            'password' => Hash::make('senai123'),
+            'role'     => 'coordenador',
+        ]);
+
     }
 }
